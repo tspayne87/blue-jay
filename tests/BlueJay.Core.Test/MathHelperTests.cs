ab159e4b5157e2fa0ba8ac55a8d98cf68eac1d2c
Terry Payne <tspayne87@gmail.com>
2021-06-17 19:22:29 -0600

    Adding in better tests for the clamp method

--- tests/BlueJay.Core.Test/MathHelperTests.cs
+++ tests/BlueJay.Core.Test/MathHelperTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace BlueJay.Core.Test
 {
@@ -10,6 +11,11 @@ namespace BlueJay.Core.Test
       Assert.Equal(5f, MathHelper.Clamp(10f, 0f, 5f));
       Assert.Equal(5f, MathHelper.Clamp(5f, 0f, 10f));
       Assert.Equal(5f, MathHelper.Clamp(-10f, 5f, 10f));
+
+      Assert.Throws<ArgumentOutOfRangeException>(() => MathHelper.Clamp(float.NaN, 0f, 5f));
+      Assert.Throws<ArgumentOutOfRangeException>(() => MathHelper.Clamp(0f, float.NaN, 5f));
+      Assert.Throws<ArgumentOutOfRangeException>(() => MathHelper.Clamp(0f, 0f, float.NaN));
+      Assert.Throws<ArgumentException>(() => MathHelper.Clamp(0f, 10f, 0f));
     }
   }
 }
