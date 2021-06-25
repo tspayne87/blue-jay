9c1a20762baa41b76b5a9896cfb3c903eb7f662d
Terry Payne <tspayne87@gmail.com>
2021-06-24 21:11:31 -0600

    Updating the replace version and getting the azure pipelines starting to build

--- replace-version.ps1
+++ replace-version.ps1
@@ -1,4 +1,12 @@
+# Get the semver version from the argument list
+$replace=$args[0];
+
 # Gather all the Nuget.csproj files and update the version information in the file
 Get-ChildItem -Path ./*.Nuget.csproj -Recurse -Force | ForEach {
-  (Get-Content $_ | ForEach {$_ -replace {{{Version}}}, $args[0]}) | Set-Content $_
+  # Log information to the host so we can check in the pipelines what is going on
+  Write-Host "Updating File: $_ | Version ($replace)"
+
+  # Get the content of the file, iterate over and replace all {{Version}} tokens, and write back the results of the replace
+  # so that all versions of the nuget packages are in sync
+  (Get-Content $_ | ForEach {$_ -replace {{{Version}}}, $replace}) | Set-Content $_
 }
