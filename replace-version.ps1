f05bd4baa2d1b4cd8398648a2b01d7dfb392ee73
Terry Payne <tspayne87@gmail.com>
2021-06-24 20:56:43 -0600

    Updating to use argument passed into script

--- replace-version.ps1
+++ replace-version.ps1
@@ -1,3 +1,4 @@
+# Gather all the Nuget.csproj files and update the version information in the file
 Get-ChildItem -Path ./*.Nuget.csproj -Recurse -Force | ForEach {
-  (Get-Content $_ | ForEach {$_ -replace {{{Version}}}, '0.1.1'}) | Set-Content $_
+  (Get-Content $_ | ForEach {$_ -replace {{{Version}}}, $args[0]}) | Set-Content $_
 }
