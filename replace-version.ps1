9c7f4a5b6aa87c71b44729dd1f88eff31daa082d
Terry Payne <tspayne87@gmail.com>
2021-06-26 16:48:36 -0600

    Updating to just use the csproj

--- replace-version.ps1
+++ replace-version.ps1
@@ -2,11 +2,11 @@
 $replace=$args[0];
 
 # Gather all the Nuget.csproj files and update the version information in the file
-Get-ChildItem -Path ./*.Nuget.csproj -Recurse -Force | ForEach-Object {
+Get-ChildItem -Path ./*.csproj -Recurse -Force | ForEach-Object {
   # Log information to the host so we can check in the pipelines what is going on
   Write-Host "Updating File: $_ | Version ($replace)"
 
   # Get the content of the file, iterate over and replace all {{Version}} tokens, and write back the results of the replace
   # so that all versions of the nuget packages are in sync
-  (Get-Content $_ | ForEach-Object {$_ -replace {{{Version}}}, $replace}) | Set-Content $_
+  (Get-Content $_ | ForEach-Object {$_ -replace {<Version>1.0.0</Version>}, "<Version>$replace</Version>"}) | Set-Content $_
 }
