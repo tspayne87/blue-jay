5bef2fd4b99a7c1c5dbf173f5caab94a374654d9
Terry Payne <tspayne87@gmail.com>
2022-02-16 21:43:13 -0600

    Adding delayed event trigger

--- lib/BlueJay.Events/Event.cs
+++ lib/BlueJay.Events/Event.cs
@@ -6,7 +6,7 @@ namespace BlueJay.Events
   /// The event object that will be sent when an event is triggered
   /// </summary>
   /// <typeparam name="TData">The data that is sent from the triggered event</typeparam>
-  internal class Event<T> : IEvent<T>
+  internal class Event<T> : IEvent<T>, IInternalEvent
   {
     /// <summary>
     /// The data that comes along with the event
@@ -29,16 +29,22 @@ namespace BlueJay.Events
     public object Target { get; private set; }
 
     /// <summary>
+    /// Timeout down till the event should be triggered
+    /// </summary>
+    public int Timeout { get; set; }
+
+    /// <summary>
     /// Constructor method to build out the event object with a easy way of doing it
     /// </summary>
     /// <param name="data">The data we are working with this event</param>
     /// <param name="target">The current target this event was triggered on</param>
-    public Event(T data, object target = null)
+    public Event(T data, object target = null, int timeout = 0)
     {
       Data = data;
       Name = data.GetType().Name;
       Target = target;
       IsComplete = false;
+      Timeout = timeout;
     }
 
     /// <summary>
